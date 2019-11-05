70760ffaffccf13881b675ef5fb365822601c926
sergio marconi <sergio.marconi@weecology.org>
2019-11-05 07:58:53 -0500

    up

--- R/get_phenetic_distance.R
+++ R/get_phenetic_distance.R
@@ -6,13 +6,16 @@
 #' @import rotl ape
 #' @examples
 #' @importFrom magrittr "%>%"
-get_cophenetic_distance <- function(species_list){
+get_cophenetic_distance <- function(species_list, sc_name = "scientificName"){
+  
+
   # Get scientific names
   species_list <- unique(species_list)
-  species_list[["scientificName"]] <- word(species_list[["scientificName"]], 1,2)
-  species_list[["scientificName"]] <- str_replace(species_list[["scientificName"]], " spp.", "")
-  species_list[["scientificName"]] <- str_replace(species_list[["scientificName"]], " sp.", "")
-  taxa <- unique(species_list[["scientificName"]])
+  species_list <- species_list[!is.na(species_list[[sc_name]]),]
+  species_list[[sc_name]] <- word(species_list[[sc_name]], 1,2)
+  species_list[[sc_name]] <- str_replace(species_list[[sc_name]], " spp.", "")
+  species_list[[sc_name]] <- str_replace(species_list[[sc_name]], " sp.", "")
+  taxa <- unique(species_list[[sc_name]])
   taxa <- taxa[!is.na(taxa)]
   taxa[taxa == "Rubus hawaiensis"] <- "Rubus"
   taxa[taxa == "Ceanothus cuneatus"] <- "Ceanothus"
@@ -22,37 +25,55 @@ get_cophenetic_distance <- function(species_list){
   taxa[taxa == "Rubus oklahomus"] <- "Rubus"
   taxa[taxa == "Rubus laciniatus"] <- "Rubus"
   taxa[taxa == "Rubus leucodermis"] <- "Rubus"
+  taxa[taxa == "Serenoa repens"] <- NA
+  taxa[taxa == "Quercus ×heterophylla"] <-" Quercus phellos"
+  taxa <- unique(taxa)
+  taxa <- taxa[!is.na(taxa)]
 
   if(length(taxa)/250<=1){
     resolved_names <- rotl::tnrs_match_names(taxa[1:length(taxa)])
   }else{
-    resolved_names <- rotl::tnrs_match_names(taxa[1:250], )
+    resolved_names <- rotl::tnrs_match_names(taxa[1:250])
     for(n in 1:floor(length(taxa)/250)){
       tmp <- rotl::tnrs_match_names(taxa[(1+n*250):min((n+1)*250, length(taxa))])
       resolved_names = rbind.data.frame(resolved_names, tmp)
     }
   }
+  species_list[[sc_name]] <- tolower(species_list[[sc_name]])
+  resolved_names = resolved_names[!is.na(resolved_names$approximate_match),]
   # format names to be linked to correct taxonIDs
   resolved_names <- dplyr::left_join(resolved_names, species_list,
-                                     by = c("unique_name" = "scientificName"))
-  resolved_names <- dplyr::group_by(resolved_names, ott_id) %>% top_n(1)
+                                     by = c("search_string" = sc_name))
+  
+  resolved_names <- dplyr::group_by(resolved_names, ott_id) %>% dplyr::slice(1)
   resolved_names = resolved_names[!is.na(resolved_names$ott_id),]
-  #get tree
-  my_tree <-  rotl::tol_induced_subtree(ott_ids = resolved_names$ott_id)
+
 
   normalize <- function(x) {
-    return ((x - min(x)) / (max(x) - min(x)))
+    return ((max(x) - x) / max(x))
   }
+  resolved_names <- resolved_names[!is.na(resolved_names$ott_id),]
+  #get tree
+  my_tree <-  rotl::tol_induced_subtree(ott_ids = resolved_names$ott_id)
+  #my_tree <- ape::compute.brlen(my_tree, 1)
+  
+  patristic_distMatrix<-ape::cophenetic.phylo(my_tree)
   #get cophenetic distance
-  cross_species_phylogenetic_correlation = 1-normalize(ape::cophenetic.phylo(my_tree))
-
+  cross_species_phylogenetic_correlation = normalize(patristic_distMatrix)
+  cross_species_phylogenetic_correlation =
+    cross_species_phylogenetic_correlation[, !colnames(cross_species_phylogenetic_correlation) 
+                                           %in% c("mrcaott31448ott33231", "Cladrastis_clade_ott7055956")]
+  cross_species_phylogenetic_correlation =
+    cross_species_phylogenetic_correlation[!rownames(cross_species_phylogenetic_correlation) 
+                                           %in% c("mrcaott31448ott33231", "Cladrastis_clade_ott7055956"), ]
   #get taxonID from ott_ids
   get_ott_id = colnames(cross_species_phylogenetic_correlation)
-  get_ott_id = sapply(strsplit(get_ott_id, "ott"), "[", 2) %>%
+  get_ott_id = sapply(strsplit(get_ott_id, "_ott"), "[", 2) %>%
     as.integer %>%
     data.frame
   taxa_id = left_join(get_ott_id, resolved_names,
-            by = c("." = "ott_id")) %>%select(taxonID)
+                      by = c("." = "ott_id")) %>%select(taxonID)
+  
   taxa_id <- taxa_id[!is.na(taxa_id)]
   # set maxtrix col and row names following taxonID
   colnames(cross_species_phylogenetic_correlation) =
--- R/get_soil_features.R
+++ R/get_soil_features.R
@@ -12,7 +12,7 @@ get_soil_features <- function(plt){
   #get position of trees and the extent the extract soil data for
   centers <- sf::st_as_sf(plt, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
 
-  bbclip <- st_bbox(centers) %>% as.numeric
+  bbclip <- sf::st_bbox(centers) %>% as.numeric
   names(bbclip) <- c("min_long", "min_lat", "max_long", "max_lat")
   bbclip = cbind.data.frame(bbclip[["min_long"]] - 0.05
                        , bbclip[["min_lat"]] - 0.05
@@ -22,11 +22,11 @@ get_soil_features <- function(plt){
 
   #bbclip <- bbclip[c(1,3,2,4)]
   #harmonize the SSURGO clip with the vegetation structure data
-  soil_geometry_to_clip <- mapunit_geom_by_ll_bbox(bbclip)
+  soil_geometry_to_clip <- soilDB::mapunit_geom_by_ll_bbox(bbclip)
   soil_geometry_to_clip <- sf::st_as_sf(soil_geometry_to_clip)
   sf::st_crs(soil_geometry_to_clip)<- 4326
   #clip
-  soil_features <- sf::st_join(centers, soil_geometry_to_clip, join = st_intersects)
+  soil_features <- sf::st_join(centers, soil_geometry_to_clip, join = sf::st_intersects)
   #soil_features$plotID<- plt$plotID
 
   return(soil_features)
